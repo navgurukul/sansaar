5f0996b51bcf016f94067f3bb7cc89cc283279c4
Rishabh Verma <me@rishabhverma.me>
2020-07-27 12:58:55 +0530

    Boilerplate added.

---
+++ server/manifest.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
