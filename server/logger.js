b00204f5720865b5573c0071387bb9c4805f04d3
anandpatel504 <anand@navgurukul.org>
2021-09-30 20:15:56 +0530

    implementing winston logger

---
+++ server/logger.js
@@ -0,0 +1,33 @@
+const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
+const config = require('config');
+
+const logFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.align(),
+  winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+const transport = new DailyRotateFile({
+  filename: config.get('logConfig.logFolder') + config.get('logConfig.logFile'),
+  datePattern: `YYYY-MM-DD-HH`,
+  zippedArchive: true,
+  maxSize: `20m`,
+  maxFiles: `14d`,
+  prepend: true,
+  level: config.get('logConfig.logLevel'),
+});
+transport.on(`rotate`, function (oldFilename, newFilename) {
+  // call function like upload to s3 or on cloud
+});
+const logger = winston.createLogger({
+  format: logFormat,
+  transports: [
+    transport,
+    new winston.transports.Console({
+      level: 'info',
+    }),
+  ],
+});
+module.exports = logger;
