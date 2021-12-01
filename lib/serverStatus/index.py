4d7bdfc49d2f26975eab4070d110257ea4d5eb03
Anand <44016225+anandpatel504@users.noreply.github.com>
2021-12-01 13:30:50 +0530

    Feat/check server status (#441)
    * Fix/adding mode col (#407)
    
    * ok;
    
    * Sending JS pathway
    
    * ok
    
    * added mode column in the users table
    
    * fixed linting errors
    
    * fixed typo
    
    * changes in the user service
    
    * removed console logs
    
    Co-authored-by: Saquib Nasim <44463854+pysaquib@users.noreply.github.com>
    Co-authored-by: pysaquib <iamsaquibnasim@gmail.com>
    
    * check server status
    
    * check-server-status
    
    * dev-server course API added
    
    * using dotenv for the credencials
    
    Co-authored-by: Saquib Nasim <44463854+pysaquib@users.noreply.github.com>
    Co-authored-by: Anand Dhakane <ananddhakane01@gmail.com>
    Co-authored-by: pysaquib <iamsaquibnasim@gmail.com>

---
+++ lib/serverStatus/index.py
@@ -0,0 +1,42 @@
+# install modules
+# pip3 install -r requirements.txt
+
+import os, requests, json
+from slack import WebClient
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path=Path(os.getcwd()).parent.parent
+load_dotenv(str(dotenv_path)+'/server/.env')
+
+# crontab instalation: (https://www.digitalocean.com/community/tutorials/how-to-use-cron-to-automate-tasks-ubuntu-1804)
+# type crontab -e on your terminal and past the down cron scheduler:
+# * * * * * python3 /home/andy/navgurukul/sansaar/lib/serverStatus/index.py > /home/andy/pydemo.txt
+
+# for resolving dotenv path
+# * * * * * cd /home/andy/navgurukul/sansaar/lib/serverStatus/ ; python3 index.py > /home/andy/pydemo.txt
+
+# send a message on slack by bot-token
+client = WebClient(
+    token=os.environ.get('slack_bot_token')
+)
+
+def send_slack_message(e):
+    response = client.chat_postMessage(
+        channel=os.environ.get('slack_channel'),
+        text="*Server is down*\n\n{}".format(e)
+    )
+    print(response)
+
+def check_server_status():
+    try:
+        url = os.environ.get('api_url')
+        response = requests.request("GET", url)
+        print(response, "response")
+        if response.status_code != 200:
+            send_slack_message("`Hey developers, there is some bug in your server please fix it ASAP`")
+        elif len(response.json())<0:
+            send_slack_message("`Hey developers, there is some bug in your server please fix it ASAP`")
+    except Exception as e:
+        send_slack_message(f'`Hey developers, there is some bug in your server please fix it ASAP`\n```${e}```')
+check_server_status()
