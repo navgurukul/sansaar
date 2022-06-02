33e612cba4c0f63cdc8917d53f405d85fe7e4708
anandpatel504 <anand@navgurukul.org>
2022-06-02 15:54:33 +0530

    added checks in scheduler

--- server/index.js
+++ server/index.js
@@ -87,45 +87,45 @@ exports.deployment = async (start) => {
   } = server.services();
 
   /* Scheduler- call calendar patch API in every 10 min*/
-  cron.schedule('00 */30 * * * *', async () => {
-    // 2 hours duration
-    const duration = UTCToISTConverter(
-      new Date(new Date().setMinutes(new Date().getMinutes() + 120))
-    );
-    const [err, nextTwoHoursClasses] = await classesService.getClassesForXXXTime(duration);
-    if (!err) {
-      for (const c of nextTwoHoursClasses) {
-        let emailList = [];
-        let userIds = [];
-        var { start_time, end_time, ..._c } = c;
-        for (const regUser of c.registrations) {
-          if (regUser.google_registration_status === false) {
-            userIds.push(regUser.user_id);
-            const [err, classRegUser] = await userService.findById(regUser.user_id);
-            if (!err) {
-              emailList.push({ email: classRegUser.email });
-            }
-            // letting google calendar deciding the previous attendees
-            let regUsers;
-            try {
-              regUsers = await calendarService.getCalendarEvent(
-                c.recurring_id !== null ? c.parent_class.calendar_event_id : c.calendar_event_id,
-                c.facilitator_id,
-                c.facilitator_email
-              );
-              if (regUsers.data.attendees) emailList.push(...regUsers.data.attendees);
-            } catch (err) {
-              logger.error(`line no-123, Calendar event error-` + JSON.stringify(err));
-              return { error: true, message: 'Calendar event error, please contact to admin' };
-            }
+  // cron.schedule('00 */30 * * * *', async () => {
+  // 2 hours duration
+  const duration = UTCToISTConverter(
+    new Date(new Date().setMinutes(new Date().getMinutes() + 120))
+  );
+  const [err, nextTwoHoursClasses] = await classesService.getClassesForXXXTime(duration);
+  if (!err) {
+    for (const c of nextTwoHoursClasses) {
+      let emailList = [];
+      let userIds = [];
+      var { start_time, end_time, ..._c } = c;
+      for (const regUser of c.registrations) {
+        if (regUser.google_registration_status === false) {
+          userIds.push(regUser.user_id);
+          const [err, classRegUser] = await userService.findById(regUser.user_id);
+          if (!err) {
+            emailList.push({ email: classRegUser.email });
+          }
+          // letting google calendar deciding the previous attendees
+          let regUsers;
+          try {
+            regUsers = await calendarService.getCalendarEvent(
+              c.recurring_id !== null ? c.parent_class.calendar_event_id : c.calendar_event_id,
+              c.facilitator_id,
+              c.facilitator_email
+            );
+            if (regUsers.data.attendees) emailList.push(...regUsers.data.attendees);
+          } catch (err) {
+            logger.error(`line no-123, Calendar event error-` + JSON.stringify(err));
+            return { error: true, message: 'Calendar event error, please contact to admin' };
           }
         }
-        // console.log(c, emailList, 'c, emailList\n\n');
-        await calendarService.patchCalendarEvent(_c, emailList);
-        await classesService.updateGRegistrationStatusById(c.id, userIds);
       }
+      // console.log(c, emailList, 'c, emailList\n\n');
+      await calendarService.patchCalendarEvent(_c, emailList);
+      await classesService.updateGRegistrationStatusById(c.id, userIds);
     }
-  });
+  }
+  // });
 
   client.start().then(() => {
     // eslint-disable-next-line
