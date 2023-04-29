4c2888690fbef3a0aab036535a0e94bc8ac68dff
giribabu22 <giribabu22@navgurukul.org>
2023-04-29 12:47:29 +0530

    first commit

--- server/plugins/swagger/index.js
+++ server/plugins/swagger/index.js
@@ -5,6 +5,37 @@ const Package = require('../../../package.json');
 const CONSTANTS = require('../../../lib/config');
 
 module.exports = {
+  // name: 'app-swagger',
+  // async register(server) {
+  //   await server.register([
+  //     Inert,
+  //     Vision,
+  //     {
+  //       plugin: HapiSwagger,
+  //       options: {
+  //         validatorUrl: null,
+  //         info: {
+  //           title: 'NG Sansaar API Docs',
+  //           version: Package.version,
+  //         },
+  //         securityDefinitions: {
+  //           jwt: {
+  //             type: 'apiKey',
+  //             name: 'Authorization',
+  //             in: 'header',
+  //           },
+  //         },
+  //         schemes: ['http', 'https'],
+  //         security: [{ jwt: [] }],
+  //         swaggerUIPath: CONSTANTS.swagger.uiPath,
+  //         jsonPath: CONSTANTS.swagger.jsonPath,
+  //         basePath: CONSTANTS.swagger.basePath,
+  //       },
+  //     },
+  //   ]);
+  // },
+
+  // <<<<<<<<<<>>>>>>>>>>>  New code ---
   name: 'app-swagger',
   async register(server) {
     await server.register([
@@ -19,14 +50,14 @@ module.exports = {
             version: Package.version,
           },
           securityDefinitions: {
-            jwt: {
+            cookieAuth: {
               type: 'apiKey',
-              name: 'Authorization',
-              in: 'header',
+              name: 'session',
+              in: 'cookie',
             },
           },
           schemes: ['http', 'https'],
-          security: [{ jwt: [] }],
+          security: [{ cookieAuth: [] }],
           swaggerUIPath: CONSTANTS.swagger.uiPath,
           jsonPath: CONSTANTS.swagger.jsonPath,
           basePath: CONSTANTS.swagger.basePath,
