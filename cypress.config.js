c7e689b9c01984795b3e84adb50f42b7f03d3ba6
jayshri15 <jayshri20@navgurukul.org>
2023-10-21 15:27:19 +0530

    Workflow added for pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2kwcbn",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
