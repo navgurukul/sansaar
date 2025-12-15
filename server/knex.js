94b3c4da67aa7af6a33df9540e3d50b3cd77074e
Mahendra <94590823+mahendra-shah@users.noreply.github.com>
2025-12-15 12:41:16 +0530

    Comment out chatKnex database configuration
    Comment out the chatKnex configuration in knex.js

--- server/knex.js
+++ server/knex.js
@@ -4,17 +4,17 @@ const knex = require('knex');
 Dotenv.config({ path: `${__dirname}/.env` });
 
 module.exports = {
-  chatKnex: knex({
-    client: 'pg',
-    connection: {
-      database: process.env.BOL_DB_NAME,
-      host: process.env.BOL_DB_HOST,
-      user: process.env.BOL_DB_USER,
-      password: process.env.BOL_DB_PASS,
-    },
-    pool: {
-      min: 4,
-      max: 200,
-    },
-  }),
+  // chatKnex: knex({
+  //   client: 'pg',
+  //   connection: {
+  //     database: process.env.BOL_DB_NAME,
+  //     host: process.env.BOL_DB_HOST,
+  //     user: process.env.BOL_DB_USER,
+  //     password: process.env.BOL_DB_PASS,
+  //   },
+  //   pool: {
+  //     min: 4,
+  //     max: 200,
+  //   },
+  // }),
 };
